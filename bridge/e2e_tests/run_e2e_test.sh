8fd6f010181b1c674d0da5a413b13b7ee384fce4
Thore Weilbier <thore@weilbier.net>
2019-06-21 13:48:24 +0200

    Fix RPC addresses and wait for reward transition

--- bridge/e2e_tests/run_e2e_test.sh
+++ bridge/e2e_tests/run_e2e_test.sh
@@ -14,6 +14,9 @@ DOCKER_COMPOSE_COMMAND="docker-compose -f ../docker-compose.yml -f docker-compos
 VALIDATOR_ADDRESS=0x46ae357bA2f459Cb04697837397eC90b47e48727 # Must be a checksum address
 VALIDATOR_ADDRESS_PRIVATE_KEY=0x0000000000000000000000000000000000000000000000000000000000000001
 PREMINTED_COINS_AMOUNT=1
+BLOCK_REWARD_CONTRACT_TRANSITION_BLOCK=5600
+NODE_SIDE_RPC_ADDRESS="http://127.0.0.1:8545"
+NODE_MAIN_RPC_ADDRESS="http://127.0.0.1:8544"
 
 OPTIND=1
 ARGUMENT_DOCKER_BUILD=0
@@ -74,13 +77,6 @@ if [[ $ARGUMENT_DOCKER_PULL == 1 ]]; then
   $DOCKER_COMPOSE_COMMAND pull
 fi
 
-echo "===> Start main and side chain node services"
-$DOCKER_COMPOSE_COMMAND up --no-start
-$DOCKER_COMPOSE_COMMAND up -d node_side node_main
-
-node_side_rpc_address="http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' node_side):8545"
-node_main_rpc_address="http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' node_main):8544"
-
 echo "===> Prepare deploy tools"
 [[ ! -d "$VIRTUAL_ENV" ]] && python3 -m venv "$VIRTUAL_ENV"
 source "$VIRTUAL_ENV/bin/activate"
@@ -91,35 +87,66 @@ echo "===> Prepare deployment tools"
 (cd "$VALIDATOR_SET_DEPLOY_DIRECTORY" && make install)
 (cd "$BRIDGE_DEPLOY_DIRECTORY" && make install)
 
-echo "===> Deploy validator set contracts"
-validator-set-deploy deploy --jsonrpc "$node_side_rpc_address" --validators "$VALIDATOR_SET_CSV_FILE"
-validator_set_proxy_contract_address=$(executeAndParseHexAddress "validator-set-deploy deploy-proxy \
-  --jsonrpc $node_side_rpc_address")
-# validator_set_proxy_contract_address=0x111
+echo "===> Start main and side chain node services"
+$DOCKER_COMPOSE_COMMAND up --no-start
+# $DOCKER_COMPOSE_COMMAND up -d node_side node_main
 
-echo "===> Deploy bridge contracts"
-foreign_bridge_contract_address=$(executeAndParseHexAddress "bridge-deploy deploy-foreign \
-  --jsonrpc $node_main_rpc_address")
+echo "===> Deploy validator set contracts"
+validator-set-deploy deploy --jsonrpc "$NODE_SIDE_RPC_ADDRESS" --validators "$VALIDATOR_SET_CSV_FILE"
+validator_set_proxy_contract_address=$(executeAndParseHexAddress "validator-set-deploy deploy-proxy \
+  --jsonrpc $NODE_SIDE_RPC_ADDRESS")
+
+echo "ValidatorSetProxy contract address: $validator_set_proxy_contract_address"
+
+echo "===> Deploy block reward contract"
 
 block_reward_contract_address=$(executeAndParseHexAddress \
-  "bridge-deploy deploy-reward --jsonrpc $node_side_rpc_address")
+  "bridge-deploy deploy-reward --jsonrpc $NODE_SIDE_RPC_ADDRESS")
+
+echo "BlockReward contract address: $block_reward_contract_address"
+
+echo "===> Deploy bridge contracts"
+
+foreign_bridge_contract_address=$(executeAndParseHexAddress "bridge-deploy deploy-foreign \
+  --jsonrpc $NODE_MAIN_RPC_ADDRESS")
+
+echo "ForeignBridge contract address: $foreign_bridge_contract_address"
 
 home_bridge_contract_address=$(executeAndParseHexAddress \
-  "bridge-deploy deploy-home --jsonrpc $node_side_rpc_address \
+  "bridge-deploy deploy-home --jsonrpc $NODE_SIDE_RPC_ADDRESS \
   --validator-set-address $validator_set_proxy_contract_address
   --block-reward-address $block_reward_contract_address
   --required-block-confirmations 1
   --owner-address $VALIDATOR_ADDRESS")
 
-echo "===> Deploy token contracts"
+echo "HomeBridge contract address: $home_bridge_contract_address"
+
+echo "===> Deploy token contract"
 token_contract_address=$(executeAndParseHexAddress "deploy-tools deploy \
-  --jsonrpc $node_main_rpc_address --contracts-dir $CONTRACT_DIRECTORY \
+  --jsonrpc $NODE_MAIN_RPC_ADDRESS --contracts-dir $CONTRACT_DIRECTORY \
   TrustlinesNetworkToken TrustlinesNetworkToken TNC 18 $VALIDATOR_ADDRESS $PREMINTED_COINS_AMOUNT")
+
+echo "Token contract address: $token_contract_address"
 
 echo "===> Set bridge environment variables"
 sed -i "s/\(FOREIGN_BRIDGE_ADDRESS=\).*/\1$foreign_bridge_contract_address/" "$ENVIRONMENT_VARIABLES_FILE"
 sed -i "s/\(HOME_BRIDGE_ADDRESS=\).*/\1$home_bridge_contract_address/" "$ENVIRONMENT_VARIABLES_FILE"
 sed -i "s/\(ERC20_TOKEN_ADDRESS=\).*/\1$token_contract_address/" "$ENVIRONMENT_VARIABLES_FILE"
+
+printf "===> Wait until block reward contract transition"
+
+blockNumber=0
+
+while [[ $blockNumber -lt $BLOCK_REWARD_CONTRACT_TRANSITION_BLOCK ]]; do
+  printf .
+  response=$(curl --silent --data \
+    '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' \
+    -H "Content-Type: application/json" -X POST $NODE_SIDE_RPC_ADDRESS)
+  blockNumberHex=$(echo "$response" | awk -F '0x' '{print $2}' | awk -F '"' '{print $1}')
+  blockNumber=$((16#$blockNumberHex))
+done
+
+printf '\n'
 
 echo "===> Start bridge services"
 
@@ -152,7 +179,7 @@ curl --data "{\"method\": \"eth_sendTransaction\", \"params\":[{ \
   \"from\": \"$VALIDATOR_ADDRESS\", \"to\": \"$token_contract_address\", \"gas\": \"0x76c0\", \
   \"gasPrice\":\"0x9184e72a000\", \"value\": \"$PREMINTED_COINS_AMOUNT\", \"data\": \"TODO\" \
   }], \"id\":1, \"jsonrpc\": \"2.0\"}" \
-  -H "Content-Type: application/json" -X POST "$node_main_rpc_address"
+  -H "Content-Type: application/json" -X POST "$NODE_MAIN_RPC_ADDRESS"
 
 echo "===> Shutting down"
 $DOCKER_COMPOSE_COMMAND down
