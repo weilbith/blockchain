1511401655b60028a2307cdf706ad640837d6c5d
Thore Weilbier <thore@brainbot.com>
2019-06-26 14:05:01 +0000

    Add transfer command and balance check

--- bridge/e2e_tests/run_e2e_test.sh
+++ bridge/e2e_tests/run_e2e_test.sh
@@ -7,13 +7,17 @@ E2E_DIRECTORY=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 VALIDATOR_SET_DEPLOY_DIRECTORY=$(realpath "$E2E_DIRECTORY/../../validator-set-deploy")
 BRIDGE_DEPLOY_DIRECTORY=$(realpath "$E2E_DIRECTORY/../../bridge-deploy")
 CONTRACT_DIRECTORY=$(realpath "$E2E_DIRECTORY/../../contracts/contracts")
+BRIDGE_DATA_DIRECTORY=$(realpath "$E2E_DIRECTORY/../bridge_data")
 VALIDATOR_SET_CSV_FILE=$(realpath "$E2E_DIRECTORY/validator-list")
 ENVIRONMENT_VARIABLES_FILE="$E2E_DIRECTORY/env_override"
 VIRTUAL_ENV="$E2E_DIRECTORY/venv"
 DOCKER_COMPOSE_COMMAND="docker-compose -f ../docker-compose.yml -f docker-compose-override.yml"
 VALIDATOR_ADDRESS=0x46ae357bA2f459Cb04697837397eC90b47e48727 # Must be a checksum address
 VALIDATOR_ADDRESS_PRIVATE_KEY=0x0000000000000000000000000000000000000000000000000000000000000001
-PREMINTED_COINS_AMOUNT=1
+TRANSFER_ACCOUNT_ADDRESS=0x7B9EeF4CC211BFBDB643ABE6D12D01599E516547
+TRANSFER_ACCOUNT_KEYFILE="$E2E_DIRECTORY/transfer-key.json"
+TRANSFER_ACCOUNT_PASSWORD="trustlines"
+PREMINTED_TOKEN_AMOUNT=1
 BLOCK_REWARD_CONTRACT_TRANSITION_BLOCK=70
 NODE_SIDE_RPC_ADDRESS="http://127.0.0.1:8545"
 NODE_MAIN_RPC_ADDRESS="http://127.0.0.1:8544"
@@ -48,8 +52,11 @@ set -o functrace # run DEBUG trap in subshells
 trap preexec DEBUG
 
 function cleanup() {
+  cwd=$(pwd)
   cd "$E2E_DIRECTORY"
   $DOCKER_COMPOSE_COMMAND down -v
+  cd $cwd
+  rm -rf $BRIDGE_DATA_DIRECTORY 
 }
 
 trap "cleanup" EXIT
@@ -68,8 +75,11 @@ function executeAndParseHexAddress() {
   echo "0x${hexAddressWithPostfix%% *}"
 }
 
-echo "===> Shutdown possible old services"
-$DOCKER_COMPOSE_COMMAND down -v
+function convertHexToDecOfJsonRpcResponse() {
+  hexResult=$(echo "$1" | awk -F '0x' '{print $2}' | awk -F '"' '{print $1}')
+  decResult=$((16#$hexResult))
+  echo $decResult
+}
 
 if [[ $ARGUMENT_DOCKER_BUILD == 1 ]]; then
   echo "===> Build images for services"
@@ -80,6 +90,9 @@ if [[ $ARGUMENT_DOCKER_PULL == 1 ]]; then
   echo "===> Pull images for services"
   $DOCKER_COMPOSE_COMMAND pull
 fi
+
+echo "===> Cleanup from previous runs"
+cleanup
 
 echo "===> Prepare deploy tools"
 [[ ! -d "$VIRTUAL_ENV" ]] && python3 -m venv "$VIRTUAL_ENV"
@@ -133,16 +146,17 @@ echo "HomeBridge contract address: $home_bridge_contract_address"
 echo "===> Deploy token contract"
 token_contract_address=$(executeAndParseHexAddress "deploy-tools deploy \
   --jsonrpc $NODE_MAIN_RPC_ADDRESS --contracts-dir $CONTRACT_DIRECTORY \
-  TrustlinesNetworkToken TrustlinesNetworkToken TNC 18 $VALIDATOR_ADDRESS $PREMINTED_COINS_AMOUNT")
+  TrustlinesNetworkToken TrustlinesNetworkToken TNC 18 $TRANSFER_ACCOUNT_ADDRESS $PREMINTED_TOKEN_AMOUNT")
 
 echo "Token contract address: $token_contract_address"
 
 echo "===> Set bridge environment variables"
-echo $foreign_bridge_contract_address
-echo $ENVIRONMENT_VARIABLES_FILE
-sed -i '' "s/\(FOREIGN_BRIDGE_ADDRESS=\).*/\1$foreign_bridge_contract_address/" "$ENVIRONMENT_VARIABLES_FILE"
-sed -i '' "s/\(HOME_BRIDGE_ADDRESS=\).*/\1$home_bridge_contract_address/" "$ENVIRONMENT_VARIABLES_FILE"
-sed -i '' "s/\(ERC20_TOKEN_ADDRESS=\).*/\1$token_contract_address/" "$ENVIRONMENT_VARIABLES_FILE"
+
+# Even if this does not change as long as the address and order of deployment isn't touched,
+# this makes sure the environment is set up 100% correctly.
+sed -i "s/\(FOREIGN_BRIDGE_ADDRESS=\).*/\1$foreign_bridge_contract_address/" "$ENVIRONMENT_VARIABLES_FILE"
+sed -i "s/\(HOME_BRIDGE_ADDRESS=\).*/\1$home_bridge_contract_address/" "$ENVIRONMENT_VARIABLES_FILE"
+sed -i "s/\(ERC20_TOKEN_ADDRESS=\).*/\1$token_contract_address/" "$ENVIRONMENT_VARIABLES_FILE"
 
 printf "===> Wait until block reward contract transition"
 
@@ -153,8 +167,7 @@ while [[ $blockNumber -lt $BLOCK_REWARD_CONTRACT_TRANSITION_BLOCK ]]; do
   response=$(curl --silent --data \
     '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' \
     -H "Content-Type: application/json" -X POST $NODE_SIDE_RPC_ADDRESS)
-  blockNumberHex=$(echo "$response" | awk -F '0x' '{print $2}' | awk -F '"' '{print $1}')
-  blockNumber=$((16#$blockNumberHex))
+  blockNumber=$(convertHexToDecOfJsonRpcResponse "$response")
   sleep 1
 done
 
@@ -200,15 +213,25 @@ printf '\n'
 
 echo "===> Test a bridge transfer from foreign to home chain"
 
+# Check balance before
+response=$(curl --silent --data "{\"method\":\"eth_getBalance\",\"params\":[\"$TRANSFER_ACCOUNT_ADDRESS\"],\"id\":1,\"jsonrpc\":\"2.0\"}" -H "Content-Type: application/json" -X POST $NODE_SIDE_RPC_ADDRESS)
+homeNativeBalanceBefore=$(convertHexToDecOfJsonRpcResponse "$response")
+echo "Balance on home chain before: $homeNativeBalanceBefore"
+
+echo "Transfer token to foreign bridge...."
+yes "$TRANSFER_ACCOUNT_PASSWORD" | deploy-tools transact --jsonrpc $NODE_MAIN_RPC_ADDRESS --contracts-dir $CONTRACT_DIRECTORY --keystore $TRANSFER_ACCOUNT_KEYFILE --contract-address $token_contract_address TrustlinesNetworkToken transfer $foreign_bridge_contract_address $PREMINTED_TOKEN_AMOUNT
+
+echo "Wait for required block confirmations and collecting signatures..."
+curl --silent --data '{"method":"eth_getTransactionReceipt","params":["0xf63af4d417d3847cc45acece2b678b9cc74a172e547a102b10eb4175d101d2be"],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST $NODE_MAIN_RPC_ADDRESS
+
+sleep 60
+
+response=$(curl --silent --data "{\"method\":\"eth_getBalance\",\"params\":[\"$TRANSFER_ACCOUNT_ADDRESS\"],\"id\":1,\"jsonrpc\":\"2.0\"}" -H "Content-Type: application/json" -X POST $NODE_SIDE_RPC_ADDRESS)
+homeNativeBalanceAfter=$(convertHexToDecOfJsonRpcResponse "$response")
+echo "Balance on home chain after: $homeNativeBalanceAfter"
+
+echo "Check the logs now you looser..."
 read
-sleep 100
-$DOCKER_COMPOSE_COMMAND logs rabbit > rabbit.log
-$DOCKER_COMPOSE_COMMAND logs redis > redis.log
-$DOCKER_COMPOSE_COMMAND logs bridge_collected > bridge_collected.log
-$DOCKER_COMPOSE_COMMAND logs bridge_affirmation > bridge_affirmation.log
-$DOCKER_COMPOSE_COMMAND logs bridge_senderhome > bridge_senderhome.log
-$DOCKER_COMPOSE_COMMAND logs bridge_senderforeign > bridge_senderforeign.log
-# TODO: wait contract-deploy-tools
 
 echo "===> Shutting down"
-$DOCKER_COMPOSE_COMMAND down
+exit 0
