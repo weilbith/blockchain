b338e18eaa121c8f8444a19a086d71bd5be56cb1
Jannik Luhn <jannik@brainbot.com>
2019-07-17 15:01:16 +0200

    Move bridge to tools sub directory

--- bridge/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="tlbc-bridge",
-    packages=find_packages(),
-    entry_points={"console_scripts": ["tlbc-bridge=bridge.main:main"]},
-)
